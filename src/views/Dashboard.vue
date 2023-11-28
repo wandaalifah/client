8c81f7bf42bd553725e77f6780fa508e21215f93
Wanda Alifah <63275714+wandaalifah@users.noreply.github.com>
2023-11-28 09:39:12 +0700

    Update Dashboard.vue

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <DashboardLayout>
     <div class="relative mx-36 mt-32 pt-10">
-        <h1 class="font-semibold text-6xl text-blue-900 w-2/3">SISTEM REKOMENDASI <br> HILIRISASI MITRA <br> RISET DAN MAGANG</h1>
+        <h1 class="font-semibold text-6xl text-blue-900 w-2/3">SISTEM REKOMENDASI <br> MITRA HILIRISASI <br> RISET DAN MAGANG</h1>
         <router-link :to="{name: 'Magang'}">
           <button class="mt-8 bg-purple-200 text-purple-700 py-2 px-4 rounded-sm font-medium">
             Dapatkan Rekomendasi
